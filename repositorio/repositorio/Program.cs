2042ebd016fc51a3d44e6b6dcdfe134a9197aecc
Pablo10118 <gogj080101hgtnnna8@cbtis217.edu.mx>
2024-10-22 16:00:35 +0200

    cree un repositorio y en el meti el nuevo proyecto

---
+++ repositorio/repositorio/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC17
+ * Date: 22/10/2024
+ * Time: 15:53
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace repositorio
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
